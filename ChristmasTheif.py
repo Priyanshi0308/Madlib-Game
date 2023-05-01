20b6abde5f3a2439db0d3ff9215969e584284a89
Priyanshi0308 <122589087+Priyanshi0308@users.noreply.github.com>
2023-05-01 19:21:19 +0530

    Add files via upload

---
+++ ChristmasTheif.py
@@ -0,0 +1,45 @@
+print('MAD LIB THEATRE')
+print("create your own Jimmy Fallon Mad Lib scene and act it out with your family and friends!")
+a= input("Type of Dessert:")
+b= input("Silly Word:")
+c= input('Another Word for Father:')
+d= input('Year:')
+e= input('Celebrity Name:')
+f= input('Type of Plant:')
+g= input('Adjective:')
+h= input('Something You’d Name a Pet:')
+i= input('Name of a Band:')
+j= input('Plural Animal:')
+k= input('Verb Ending in -Ing:')
+l= input('Number:')
+m= input('Measurement of Time:')
+n= input('Body Part:')
+o= input('Type of Beer:')
+p= input('Type of Doctor:')
+q= input('One Syllable Noise:')
+r= input('Plural Noun:')
+s= input("Mode of Transportation:")
+t= input("Holiday:")
+SCENE= f'ELF: Hi Santa.\
+SANTA: Hello {a}.\
+ELF: You’re looking at me like I’m on your naughty list.\
+SANTA: I was counting my toys and was one {b} short. I believe you stole it.\
+ELF: But {c}!\
+SANTA: I started making that toy in {d}. What will little {e} think when he/she wakes up on Christmas morning with nothing under the {f}?\
+ELF: I would never steal such a {g} toy.\
+SANTA: Where were you the night before Christmas?\
+ELF: I was with {h}. We were listening to {i}’s holiday album and making snow {j}.\
+SANTA: Then why did Rudolph see you {k} on the roof {l} {m} before the toy went missing?\
+ELF: That’s a coincidence.\
+SANTA: The reason I work so hard, is because I get to spread joy to millions of people, and go sit by the fire shaking my {n} while eating cookies and drinking {o}.\
+ELF: Fine, I stole the toy. But I only did it because I wanted to impress my {p}.\
+SANTA: {q},{q},{q} Every time there’s a holiday miracle, I sing my favorite Christmas song, Jingle {r}.\
+(singing)\
+Jingle {r}, Jingle {r},\
+Jingle all the way.\
+Oh what fun it is to ride\
+In a one horse open {s}.\
+ELF Merry {t} Santa!\
+(BEAT)\
+\
+And scene!'
